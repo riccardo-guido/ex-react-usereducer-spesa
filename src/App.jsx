ea38bfec721dce74ed4fd79dce65fe6131697987
riccardo-guido <guidoriccardo02@gmail.com>
2025-09-02 13:05:03 +0200

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   const products = [
     { name: "Mela", price: 0.5 },
@@ -5,6 +7,24 @@ function App() {
     { name: "Latte", price: 1.0 },
     { name: "Pasta", price: 0.7 },
   ];
+
+  const [addedProducts, setAddedProducts] = useState([]);
+  const addToCart = (product) => {
+    const isProductAlreadyAdded = addedProducts.some(
+      (p) => p.name === product.name
+    );
+    if (isProductAlreadyAdded) {
+      return;
+    }
+
+    setAddedProducts((curr) => [
+      ...curr,
+      {
+        ...product,
+        quantity: 1,
+      },
+    ]);
+  };
 
   return (
     <>
@@ -14,10 +34,26 @@ function App() {
           <li key={i}>
             <p>
               {p.name} ({p.price.toFixed(2)}€)
+              <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
             </p>
           </li>
         ))}
       </ul>
+      {addedProducts.length > 0 && (
+        <>
+          {" "}
+          <h2>Carrello</h2>
+          <ul>
+            {addedProducts.map((p, i) => (
+              <li key={i}>
+                <p>
+                  {p.quantity} x {p.name} ({p.price.toFixed(2)}€)
+                </p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 }
